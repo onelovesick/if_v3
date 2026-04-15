e1d750943bf613ac91169eacfbeac57af0196e77
onelovesick <yousifjabak74@gmail.com>
2026-04-14 21:14:07 -0400

    2

--- src/components/TopScene.tsx
+++ src/components/TopScene.tsx
@@ -1,12 +1,10 @@
 import Hero from "@/components/Hero";
-import Problem from "@/components/Problem";
 import styles from "./TopScene.module.css";
 
 export default function TopScene() {
   return (
     <div className={styles.scene}>
       <Hero />
-      <Problem />
     </div>
   );
 }
